3a2f1a2e8e213578abb7922e96601b946e89d8eb
Lê Hoàng Thoại <lhthoai@LeHoangThoai.local>
2026-06-03 11:32:21 +0700

    Chỉnh sửa validation

--- src/main/java/com/quanlycafe/cafe_management/dto/EmployeeFormDTO.java
+++ src/main/java/com/quanlycafe/cafe_management/dto/EmployeeFormDTO.java
@@ -3,6 +3,7 @@ package com.quanlycafe.cafe_management.dto;
 import com.quanlycafe.cafe_management.validation.ValidPhoneNumber;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -34,6 +35,12 @@ public class EmployeeFormDTO {
     @NotNull(message = "Vui lòng chọn chức vụ")
     private Integer maChucVu;
 
+    @NotBlank(message = "Tên đăng nhập không được để trống")
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Tên đăng nhập viết liền không dấu, không khoảng trắng")
     private String tenDangNhap;
+
+    @NotBlank(message = "Mật khẩu không được để trống")
+    @Size(min = 6, message = "Mật khẩu phải có ít nhất 6 ký tự")
+    @Pattern(regexp = "^[\\x20-\\x7E]+$", message = "Mật khẩu không được chứa ký tự tiếng Việt có dấu")
     private String matKhau;
 }
