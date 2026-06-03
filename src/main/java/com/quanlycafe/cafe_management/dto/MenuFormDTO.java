bb8cfa0215770bcd0c240898dd895d65c3d8b765
Lê Hoàng Thoại <lhthoai@LeHoangThoai.local>
2026-06-03 19:24:59 +0700

    Chỉnh sửa

--- src/main/java/com/quanlycafe/cafe_management/dto/MenuFormDTO.java
+++ src/main/java/com/quanlycafe/cafe_management/dto/MenuFormDTO.java
@@ -1,9 +1,6 @@
 package com.quanlycafe.cafe_management.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 /**
